cd82b0b83188885dfb22ad5031a0674eede563ad
19Mamedov <163626907+19Mamedov@users.noreply.github.com>
2024-05-11 09:55:55 +0400

    Array

---
+++ script.js
@@ -0,0 +1,17 @@
+let arr = [23,12,20,10,2]
+let array = [10,2,5,3,4]
+function length(a,b) {
+    if (arr.length == array.length) {
+        for (let i = 0 ; i < a.length ; i++) {
+          let c = arr[i] * array [i]
+          console.log(c)
+        }
+        
+        
+    }
+    else {
+        console.log("Xeta");
+    }
+    
+}
+length (arr,array)
